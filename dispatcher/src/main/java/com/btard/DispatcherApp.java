a6489707444b6e468335896f31c2b39782c5ba7f
Dee <dima.irqtsk@yandex.ru>
2023-09-29 19:15:07 +0800

    Initial commit

---
+++ dispatcher/src/main/java/com/btard/DispatcherApp.java
@@ -0,0 +1,11 @@
+package com.btard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DispatcherApp.class);
+    }
+}
