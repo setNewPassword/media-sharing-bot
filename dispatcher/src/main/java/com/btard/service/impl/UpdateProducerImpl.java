e7de5ba644ef7299818db0297f34a65bca44d9f1
Dee <dima.irqtsk@yandex.ru>
2023-09-30 13:45:01 +0800

    Feat: add new module

---
+++ dispatcher/src/main/java/com/btard/service/impl/UpdateProducerImpl.java
@@ -0,0 +1,15 @@
+package com.btard.service.impl;
+
+import com.btard.service.UpdateProducer;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+@Log4j
+public class UpdateProducerImpl implements UpdateProducer {
+    @Override
+    public void produce(String rabbitQueue, Update update) {
+        log.debug(update.getMessage().getText());
+    }
+}
---
+++ dispatcher/src/main/java/com/btard/utils/MessageUtils.java
@@ -0,0 +1,17 @@
+package com.btard.utils;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class MessageUtils {
+    public  SendMessage generateSendMessageWithText(Update update, String text) {
+        var message = update.getMessage();
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId());
+        sendMessage.setText(text);
+
+        return sendMessage;
+    }
+}
